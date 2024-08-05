873534fcaaf64d0eac6cb7f30d6196a7bfa52605
consoli.log <eumsoli@gmail.com>
2024-08-06 04:02:55 +0900

    :tada: Begin hello-spring-core

---
+++ src/main/java/soli/hello_spring_core/HelloSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package soli.hello_spring_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soli/hello_spring_core/HelloSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package soli.hello_spring_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
